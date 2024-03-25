037a2c21d5463cf962a8f8e800046d1a62404852
@JaedsonJunior <jaedsonservice@gmail.com>
2024-03-25 14:30:45 -0300

    .h funcionario

---
+++ funcionario.h
@@ -0,0 +1,13 @@
+#ifndef funcionario_h
+#define funcionario_h
+
+
+int tela_menu_funcionario(void);
+void tela_cadastrar_funcionario(void);
+void tela_pesquisar_funcionario(void);
+void tela_alterar_funcionario(void);
+void tela_excluir_funcionario(void);
+
+
+
+#endif
