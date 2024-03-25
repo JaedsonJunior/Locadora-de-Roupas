af008f5ab1dcf0517dae29dc050a788bb2f3b0e9
@JaedsonJunior <jaedsonservice@gmail.com>
2024-03-25 14:31:03 -0300

    switch produto

--- produto.c
+++ produto.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "produto.h"
 
 
-
-void tela_menu_produto(void) {
+int tela_menu_produto(void) {
+    int op;
     system("clear||cls");
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
@@ -23,17 +24,39 @@ void tela_menu_produto(void) {
     printf("///                                                                         ///\n");
     printf("///            1. Cadastrar um novo produto                                 ///\n");
     printf("///            2. Pesquisar os dados de um produto                          ///\n");
-    printf("///            3. Atualizar os dados de um produto                         ///\n");
+    printf("///            3. Atualizar os dados de um produto                          ///\n");
     printf("///            4. Excluir um produto do sistema                             ///\n");
     printf("///            0. Voltar ao menu anterior                                   ///\n");
     printf("///                                                                         ///\n");
     printf("///            Escolha a opcao desejada: ");
+    scanf("%d",&op);
     printf("///                                                                         ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-    
+    switch (op) {
+            case 1:
+                tela_cadastrar_produto();
+                break;
+            case 2:
+                tela_pesquisar_produto();
+                break;
+            case 3:
+                tela_alterar_produto();
+                break;
+            case 4:
+                tela_excluir_produto();    
+                break;
+            case 0:
+                printf("saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida\n");
+                break;
+        }
+
+return op;        
 }
 
 void tela_cadastrar_produto(void) {
