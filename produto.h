a9c640b1a698eec6ac26adf5fca2cebe756ab782
@JaedsonJunior <jaedsonservice@gmail.com>
2024-03-25 14:32:20 -0300

    .h produto

---
+++ produto.h
@@ -0,0 +1,12 @@
+#ifndef produto_h
+#define produto_h
+
+
+int tela_menu_produto(void);
+void tela_cadastrar_produto(void);
+void tela_pesquisar_produto(void);
+void tela_alterar_produto(void);
+void tela_excluir_produto(void);
+
+
+#endif
