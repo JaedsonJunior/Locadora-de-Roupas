9ccf049fe5b7994a75f2ddc8827b18923b96b9f2
@JaedsonJunior <jaedsonservice@gmail.com>
2024-03-25 14:30:16 -0300

    .h cliente

---
+++ cliente.h
@@ -0,0 +1,15 @@
+#ifndef cliente_h
+#define cliente_h
+
+
+int tela_menu_cliente();
+void tela_cadastrar_cliente(void);
+void tela_pesquisar_cliente(void);
+void tela_alterar_cliente(void);
+void tela_excluir_cliente(void);
+
+
+
+
+
+#endif
