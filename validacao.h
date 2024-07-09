fa23293d0942557e389da05fd7cc3248804aaf65
@JaedsonJunior <jaedsonservice@gmail.com>
2024-07-09 07:16:57 -0300

    asinaturas_validacao

--- validacao.h
+++ validacao.h
@@ -5,5 +5,9 @@ int ehLetra(char c);
 int va_numero(char* c);
 int validarCPF(const char *cpf);
 int validaNome(const char nome[]);
+int validaFone(const char fone[]);
+int validaEmail(const char *email);
+int extrair_data(const char *data, int *dia, int *mes, int *ano);
+int valida_data(const char *data);
 
 #endif
