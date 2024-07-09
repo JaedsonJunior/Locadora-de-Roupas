5dd15ccf415bea923a888845efb147788731703e
@JaedsonJunior <jaedsonservice@gmail.com>
2024-07-09 07:50:40 -0300

    struct_base

--- cliente.h
+++ cliente.h
@@ -1,6 +1,15 @@
 #ifndef cliente_h
 #define cliente_h
+typedef struct cliente Cliente;
 
+struct cliente
+{
+char cpf[12];
+char nome[61];
+char email[61];
+char data[12];
+char fone[15]; 
+};
 
 int tela_menu_cliente();
 void tela_cadastrar_cliente(void);
