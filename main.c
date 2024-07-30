8dc46dcffc157b144b9b046e6bb4658aa29807cb
@JaedsonJunior <jaedsonservice@gmail.com>
2024-07-30 00:09:09 -0300

    final

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include "funcionario.h"
 #include "produto.h"
 #include "ultilidade.h"
+#include "mdvenda.h"
+#include "relatorio.h"
 int menu_tela_principal();
 
 int main(){
@@ -21,11 +23,13 @@ int menu_tela_principal(void){
     int opcaoCl;
     int opcaoC2;
     int opcaoC3;
+    int opcaoC4;
+    int opcaoC5;
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
     printf("///            ===================================================          ///\n");
     printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///                  = = = =   Locadora de Roupa     = = = =                ///\n");
+    printf("///                  = = = =   Supply clother    = = = =                    ///\n");
     printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
     printf("///            ===================================================          ///\n");
     printf("///                                                                         ///\n");
@@ -37,6 +41,8 @@ int menu_tela_principal(void){
     printf("///            1. Cliente                                                   ///\n");
     printf("///            2. Funcionario                                               ///\n");
     printf("///            3. Produto                                                   ///\n");
+    printf("///            4. Venda                                                     ///\n");
+    printf("///            5. relatorio                                                 ///\n");
     printf("///            0. Sair                                                      ///\n");
     printf("///                                                                         ///\n");
     printf("///            Escolha a opcaoo desejada: ");
@@ -64,6 +70,18 @@ int menu_tela_principal(void){
             {
             opcaoC3=tela_menu_produto(); 
             } while (opcaoC3!=0);
+            break;
+     case 4:
+        do
+            {
+            opcaoC4=tela_menu_venda(); 
+            } while (opcaoC4!=0);
+            break;        
+     case 5:
+        do
+            {
+            opcaoC5=relatorios(); 
+            } while (opcaoC5!=0);
             break;
     case 0:
         printf("Saindo...");
